07164a56db1c47762aa490d9e68243505cff40ba
Moacir Petry <m.petry9@gmail.com>
2018-10-25 18:19:22 -0300

    implemented the function total in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,19 @@
 class Order < ApplicationRecord
+
+  # Associations
   belongs_to :customer
   belongs_to :user
   belongs_to :table
+  has_many :order_products, inverse_of: :order
+  has_many :products, through: :order_products
 
   # Validates
   validates :user, :customer, :table, presence: true
 
   # Calculate the total of prices in order
-  # def total_order
-  #   total = products.to_a.sum { |product| product.price }
-  #   Order.update(total: total)
-  #   total
-  # end
+  def total_order
+    total = products.to_a.sum { |product| product.price }
+    Order.update(total: total)
+    total
+  end
 end
