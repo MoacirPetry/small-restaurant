d6d2f4f828c58a2df8ea6b20270fa948dfe13f31
Moacir Petry <m.petry9@gmail.com>
2019-04-23 22:57:24 -0300

    categories controller test implemented

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -3,4 +3,7 @@ FactoryBot.define do
     name { Faker::Lorem.word   }
     description { Faker::Lorem.paragraphs }
   end
+  factory :invalid_category, class: Category do
+    name { nil }
+  end
 end
