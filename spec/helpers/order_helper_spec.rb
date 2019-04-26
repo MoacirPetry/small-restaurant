1501b42e7680ddd86f669abd5131859a45aa946c
Moacir Petry <m.petry9@gmail.com>
2019-04-26 18:02:00 -0300

    tested the helpers

--- spec/helpers/order_helper_spec.rb
+++ spec/helpers/order_helper_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe OrderHelper, type: :helper do
 
-  it { expect(completed(true)).to eq("Completed").or eq("Open") }
+  context 'when completed function is called' do
+    it "true?" do
+      expect(completed(true)).to eq("Completed")
+    end
+    it "false?" do
+      expect(completed(false)).to eq("Open")
+    end
+    it "!true?" do
+      expect(completed(false)).not_to eq("Completed")
+    end
+    it "!false?" do
+      expect(completed(true)).not_to eq("Open")
+    end
+    it "!nil?" do
+      expect(completed(false)).not_to eq(nil)
+    end
+    it "param?" do
+      expect{ completed }.to raise_error(ArgumentError)
+    end
+  end
 
 end
--- spec/helpers/tables_helper_spec.rb
+++ spec/helpers/tables_helper_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe TablesHelper, type: :helper do
 
-  it { expect(availability(false)).to eq("Busy").or eq("Free") }
+  context 'when availability function is called' do
+    it "true?" do
+      expect(availability(true)).to eq("Busy")
+    end
+    it "false?" do
+      expect(availability(false)).to eq("Free")
+    end
+    it "!true?" do
+      expect(availability(false)).not_to eq("Busy")
+    end
+    it "!false?" do
+      expect(availability(true)).not_to eq("Free")
+    end
+    it "!nil?" do
+      expect(availability(false)).not_to eq(nil)
+    end
+    it "!param?" do
+      expect{ availability }.to raise_error(ArgumentError)
+    end
+  end
 
 end
