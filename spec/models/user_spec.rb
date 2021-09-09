0cfa25dc423959555a2ab0b08e8a87af92a7dd40
Moacir Petry <m.petry9@gmail.com>
2021-09-09 17:47:03 -0300

    removed double quotes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  context "Test New User View" do
-    it "Create a new user" do
+  context 'Test New User View' do
+    it 'Create a new user' do
       users = User.all
       size_old = users.size
       visit '/users/sign_up'
       within('#new_user') do
-        fill_in 'Email', :with => "jason@usa.com"
-        fill_in 'Password', :with => "jason@usa.com"
-        fill_in 'Password confirmation', :with => "jason@usa.com"
+        fill_in 'Email', with: 'jason@usa.com'
+        fill_in 'Password', with: 'jason@usa.com'
+        fill_in 'Password confirmation', with: 'jason@usa.com'
       end
       click_button 'Sign up'
       expect(page).to have_content 'successfully'
@@ -19,9 +19,9 @@ RSpec.describe User, type: :model do
     end
   end
 
-  context "Associations" do
-    it "have_many?" do
-      is_expected.to have_many(:orders)#have_many(shoulda-matchers) has_many(rails)
+  context 'Associations' do
+    it 'have_many?' do
+      is_expected.to have_many(:orders)
     end
   end
 
