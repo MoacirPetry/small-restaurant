0c57ff36dc0c6502eac2d04958e70e3998e719bb
Moacir Petry <m.petry9@gmail.com>
2021-09-10 11:55:50 -0300

    removed the double quotes

--- spec/helpers/tables_helper_spec.rb
+++ spec/helpers/tables_helper_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe TablesHelper, type: :helper do
 
   context 'when availability function is called' do
-    it "true?" do
-      expect(availability(true)).to eq("Busy")
+    it 'true?' do
+      expect(availability(true)).to eq('Busy')
     end
-    it "false?" do
-      expect(availability(false)).to eq("Free")
+    it 'false?' do
+      expect(availability(false)).to eq('Free')
     end
-    it "!true?" do
-      expect(availability(false)).not_to eq("Busy")
+    it '!true?' do
+      expect(availability(false)).not_to eq('Busy')
     end
-    it "!false?" do
-      expect(availability(true)).not_to eq("Free")
+    it '!false?' do
+      expect(availability(true)).not_to eq('Free')
     end
-    it "!nil?" do
+    it '!nil?' do
       expect(availability(false)).not_to eq(nil)
     end
-    it "!param?" do
+    it '!param?' do
       expect{ availability }.to raise_error(ArgumentError)
     end
   end
