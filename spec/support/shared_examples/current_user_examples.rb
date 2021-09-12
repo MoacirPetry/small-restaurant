055b089056d469b18831bf5324348b4a3b991fc5
Moacir Petry <m.petry9@gmail.com>
2021-09-12 10:04:59 -0300

    removed double quotes

--- spec/support/shared_examples/current_user_examples.rb
+++ spec/support/shared_examples/current_user_examples.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples :current_user_examples do
   context 'Checking current_user' do
-    it "should have a current_user" do
+    it 'should have a current_user' do
       expect(subject.current_user).to_not eq(nil)
     end
   end
--- spec/support/shared_examples/new_examples.rb
+++ spec/support/shared_examples/new_examples.rb
@@ -1,9 +1,9 @@
 RSpec.shared_examples :new_examples do
   subject { get :new }
-  it "render the new template" do
+  it 'render the new template' do
     expect(subject).to render_template(:new)
   end
-  it "returns http success" do
+  it 'returns http success' do
     expect(subject).to have_http_status(:success)
   end
 end
