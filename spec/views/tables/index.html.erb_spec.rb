11eb0e44978536967e9cf451fae01bf1695776c5
Moacir Petry <m.petry9@gmail.com>
2022-08-04 10:43:04 -0300

    Fix rubocop info

--- spec/views/tables/index.html.erb_spec.rb
+++ spec/views/tables/index.html.erb_spec.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'tables/index.html.erb', type: :view do
-
   let(:user) { FactoryBot.create(:user) }
 
   before(:each) do
     @table = FactoryBot.create(:table)
-    login_as(user, :scope => :user)
+    login_as(user, scope: :user)
   end
 
   context 'GET #index' do
@@ -20,22 +21,21 @@ RSpec.describe 'tables/index.html.erb', type: :view do
       expect(page).to have_content('New Table')
     end
     it 'GET #new' do
-      expect(page).to have_link('New Table', :href => new_table_path)
+      expect(page).to have_link('New Table', href: new_table_path)
     end
     it 'GET #edit' do
-      expect(page).to have_link('Edit', :href => edit_table_path(@table))
+      expect(page).to have_link('Edit', href: edit_table_path(@table))
     end
 
     it 'DELETE #destroy' do
-      expect(page).to have_link('Delete', :href => table_path(@table))
+      expect(page).to have_link('Delete', href: table_path(@table))
     end
   end
 
   describe 'invalid link' do
     it 'GET #show' do
       visit tables_path
-      expect(page).not_to have_link('Show', :href => table_path(@table))
+      expect(page).not_to have_link('Show', href: table_path(@table))
     end
   end
-
 end
